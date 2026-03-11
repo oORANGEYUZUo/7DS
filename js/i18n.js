fa902caa6ac510c3c8dd9ecc3d8beee8f437e7eb
oORANGEYUZUo <103068773+oORANGEYUZUo@users.noreply.github.com>
2026-03-11 21:40:18 +0700

    Update i18n.js

--- js/i18n.js
+++ js/i18n.js
@@ -202,16 +202,15 @@ var I18n = (function () {
     },
 
     th: {
-      // Navbar
-      nav_home: '\u0E2B\u0E19\u0E49\u0E32\u0E41\u0E23\u0E01',
-      nav_characters: '\u0E15\u0E31\u0E27\u0E25\u0E30\u0E04\u0E23',
-      nav_tierlist: '\u0E08\u0E31\u0E14\u0E2D\u0E31\u0E19\u0E14\u0E31\u0E1A',
-      nav_events: '\u0E2D\u0E35\u0E40\u0E27\u0E19\u0E15\u0E4C',
-      nav_admin: '\u0E41\u0E2D\u0E14\u0E21\u0E34\u0E19',
-      nav_login: '\u0E40\u0E02\u0E49\u0E32\u0E2A\u0E39\u0E48\u0E23\u0E30\u0E1A\u0E1A',
-      nav_logout: '\u0E2D\u0E2D\u0E01\u0E08\u0E32\u0E01\u0E23\u0E30\u0E1A\u0E1A',
-      nav_dashboard: '\u0E41\u0E14\u0E0A\u0E1A\u0E2D\u0E23\u0E4C\u0E14',
-      nav_settings: '\u0E15\u0E31\u0E49\u0E07\u0E04\u0E48\u0E32',
+     // Navbar
+     nav_home: '\u0E2B\u0E19\u0E49\u0E32\u0E2B\u0E25\u0E31\u0E01',
+     nav_characters: '\u0E15\u0E31\u0E27\u0E25\u0E30\u0E04\u0E23',
+     nav_weapons: '\u0E2D\u0E32\u0E27\u0E38\u0E18',
+     nav_tierlist: '\u0E08\u0E31\u0E14\u0E2D\u0E31\u0E19\u0E14\u0E31\u0E1A',
+     nav_events: '\u0E2D\u0E35\u0E40\u0E27\u0E19\u0E15\u0E4C',
+     nav_guides: '\u0E44\u0E01\u0E14\u0E4C',
+     nav_admin: '\u0E41\u0E2D\u0E14\u0E21\u0E34\u0E19',
+     nav_login: '\u0E40\u0E02\u0E49\u0E32\u0E23\u0E30\u0E1A\u0E1A',
 
       // Hero
       hero_badge: '\u0E10\u0E32\u0E19\u0E02\u0E49\u0E2D\u0E21\u0E39\u0E25\u0E41\u0E1F\u0E19\u0E40\u0E21\u0E14',
@@ -495,10 +494,3 @@ if (document.readyState === 'loading') {
 } else {
   I18n.init();
 }
-
-nav_home: 'Home',
-nav_characters: 'Characters',
-nav_weapons: 'Weapons',
-nav_tierlist: 'Tier List',
-nav_events: 'Events',
-nav_guides: 'Guides',
